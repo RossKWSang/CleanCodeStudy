e897750df8b368c216204e6ea169e98ff9b1679f
Wonsang Kim <rosskysoss@gmail.com>
2023-10-13 10:12:19 +0900

    ch2: meaningful context

---
+++ src/main/java/example/ch02/Index.md
@@ -0,0 +1,6 @@
+- 의도를 분명히 밝혀라.
+  - int d;
+  - int elapsedTimeInDays;
+
+- 그릇된 정보를 피하라
+  - 
--- src/main/java/example/ch02/MeaningfulContext.java
+++ src/main/java/example/ch02/MeaningfulContext.java
@@ -1,2 +1,77 @@
-package example.ch02;public class MeaningfulContext {
+package example.ch02;
+
+public class MeaningfulContext {
+    private static String number;
+    private static String verb;
+    private static String pluralModifier;
+
+    /* 맥락이 불분명?
+    *
+    *
+    * */
+    private static void printGuessStatisticsWrongCase(char candidate, int count) {
+        if (count==0) {
+            number="no";
+            verb="are";
+            pluralModifier="s";
+        } else if (count==1) {
+            number="1";
+            verb="is";
+            pluralModifier="";
+        } else {
+            number=Integer.toString(count);
+            verb="are";
+            pluralModifier="s";
+        }
+        String guessMessage=String.format(
+          "There %s %s %s%s", verb, number, candidate, pluralModifier
+        );
+        System.out.println(guessMessage);
+    }
+
+
+    /* 먁락이 분명한 함수
+    *
+    * */
+
+    private static String printGuessStatisticsRightCase(char candidate, int count) {
+        createPluralDependentMessageParts(count);
+        return String.format(
+                "There %s %s %s%s",
+                verb, number, candidate, pluralModifier
+        );
+    }
+
+    private static void createPluralDependentMessageParts(int count) {
+        if (count==0) {
+            thereAreNoLetters();
+        } else if (count==1) {
+            thereIsOneLetter();
+        } else {
+            thereAreManyLetters(count);
+        }
+    }
+
+    private static void thereAreManyLetters(int count) {
+        number=Integer.toString(count);
+        verb="are";
+        pluralModifier="s";
+    }
+
+    private static void thereIsOneLetter() {
+        number="1";
+        verb="is";
+        pluralModifier="";
+    }
+
+    private static void thereAreNoLetters() {
+        number="no";
+        verb="are";
+        pluralModifier="s";
+    }
+
+    public static void main(String[] args) {
+        printGuessStatisticsWrongCase('A', 3);
+        System.out.println(printGuessStatisticsRightCase('B', 0));
+    }
 }
